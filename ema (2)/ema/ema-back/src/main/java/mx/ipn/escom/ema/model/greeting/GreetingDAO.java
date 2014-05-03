c73d70c8ad296150f0e417671ef3c94b01c36e8b
elviuxiboo <eandrea.gdiaz@gmail.com>
2014-05-02 23:03:16 -0500

    Revert "DAO Y SERVICES"
    This reverts commit ada92dcbddef567c0adda07b0c4602dba56bb469.

--- ema (2)/ema/ema-back/src/main/java/mx/ipn/escom/ema/model/greeting/GreetingDAO.java
+++
@@ -1,9 +0,0 @@
-package mx.ipn.escom.ema.model.greeting.dao;
-
-import mx.ipn.escom.ema.model.entities.Greeting;
-import java.util.List;
-
-public interface GreetingDAO{    
-    List<Greeting> getAll();
-    void save(Greeting greeting);
-}
--- ema (2)/ema/ema-services/src/main/java/mx/ipn/escom/ema/services/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package mx.ipn.escom.ema.services;
-
-import mx.ipn.escom.ema.to.GreetingTO;
-import java.util.List;
-
-public interface GreetingService{    
-    public void saveGreeting(GreetingTO greetingTO);
-    public List<GreetingTO> getGreetings();
-}
